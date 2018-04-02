ed46249209a9bb4a423f05a9d42af04fae9ba007
Leonamquintao <leonamquintao@gmail.com>
2018-04-02 15:20:11 -0300

    add animations

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="home">
+    <div class="container">
 
-    <h1>Filter & Mixins</h1>
-    <p>filter register locally => {{ text | toUppercase }}</p>
-    <p>filter register globally => {{ msg | toLowercase }}</p>
-    <hr>
-    <input type="text" v-model="filterText">
+      <h1>Filter, Mixins and Animations</h1>
 
-    <ul>
-      <li v-for="quality in filteredQualities">{{ quality }}</li>
-    </ul>
+      <div class="row">
+
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h2>Animations</h2>
+          <hr>
+          <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+          <button class="btn btn-danger" @click="showSlide = !showSlide">Show Danger</button>
+          <br><br>
+
+          <transition name="fade">
+            <div class="alert alert-info" v-if="show"> this is some info</div>
+          </transition>
+
+          <transition name="slide">
+            <div class="alert alert-danger" v-if="showSlide"> this is some Danger</div>
+          </transition>
+
+        </div>
+
+      </div>
+
+      <p>filter register locally => {{ text | toUppercase }}</p>
+      <p>filter register globally => {{ msg | toLowercase }}</p>
+
+      <hr>
+
+      <input type="text" v-model="filterText">
+
+      <ul>
+        <li v-for="quality in filteredQualities">{{ quality }}</li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
@@ -17,6 +44,8 @@
 export default {
   data () {
     return {
+      show: false,
+      showSlide: false,
       text: 'this is a awesome day!',
       msg: 'BUT UNFORTUNATELY IS ENDING!',
       qualities: ['hot', 'cold', 'dark', 'clear', 'warm', 'cloudy'],
@@ -44,8 +73,57 @@ export default {
     height: auto;
     margin: auto;
   }
+
   .home h1{
     text-align: center;
+    padding-bottom: 20px;
+  }
+
+  .fade-enter {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+  .fade-leave {
+
+  }
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+  }
+  .slide-enter {
+    opacity: 0;
+  }
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+  }
+  .slide-leave {
+    /*  no inital state */
+  }
+  .slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+    transition: opacity 1s;
+    opacity: 0;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
   }
 
 </style>
